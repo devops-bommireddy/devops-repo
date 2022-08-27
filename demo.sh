df6bd81c180bc8a8a785daef8e5018532d1b5cd3
devops-bommireddy <yesuprasad7043@gmail.com>
2022-08-27 17:03:35 +0530

    update demo bash file

--- demo.sh
+++ demo.sh
@@ -2,4 +2,6 @@ echo "************* display root file system space**************"
 df -h 
 echo "*************display today date ****************"
 date
+echo "*************display jenkins memory space *****"
+free -m
 
