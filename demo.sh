0b4894d7b657d9bff6e43ed53532efe953595280
devops-bommireddy <yesuprasad7043@gmail.com>
2022-11-21 15:08:16 +0530

    create bash file

--- demo.sh
+++ demo.sh
@@ -1,7 +1,7 @@
-<<<<<<< HEAD
+
 #!/bin/bash
 echo "**********display root f.s space**********"
 df -h
-echo "**********display today date *************date
+echo "**********display today date *************"
 date
 
