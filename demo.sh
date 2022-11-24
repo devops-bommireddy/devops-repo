85b005c9ad57d17dc1a81e76709102103c039777
devops-bommireddy <yesuprasad7043@gmail.com>
2022-11-24 14:28:44 +0530

    create jenkins file

--- demo.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-echo "***********display root f.s space**********"
-df -h
-
-echo"***********display today date**************"
-date
-
---
+++ jenkins.sh
@@ -0,0 +1,7 @@
+#! /bin/bash/
+echo "**************** display root f.s space************"
+df -h
+
+echo "***************** display today date **************"
+date
+
